b6af00bfc8cd3a88859fd80db4eeb9f3e1b3befb
RTerabyte <rodry.ter@gmail.com>
2026-05-15 12:25:33 +0200

    refactor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/guards/auth.guard';
 import { adminGuard } from './core/auth/guards/admin.guard';
 
-
 export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./features/identity/ui/login/login/login.component').then((m) => m.LoginComponent),
+      import('./features/identity/ui/login/login/login.component').then(
+        (m) => m.LoginComponent,
+      ),
   },
   {
     path: 'tpv',
